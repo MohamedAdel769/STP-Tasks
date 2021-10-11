15b33b1afa2c0e066a7c696899975b9e92a99d64
Mohamed Adel <Mohamed.adel90211@gmail.com>
2021-10-12 00:25:38 +0200

    Seperated tables creation file

--- jpa-task5/src/main/java/org/example/App.java
+++ jpa-task5/src/main/java/org/example/App.java
@@ -4,11 +4,6 @@ import entities.Employee;
 import entities.Role;
 import org.example.repository.EmployeeRepository;
 
-import javax.persistence.EntityManager;
-import javax.persistence.EntityManagerFactory;
-import javax.persistence.EntityTransaction;
-import javax.persistence.Persistence;
-import java.util.ArrayList;
 import java.util.List;
 import java.util.Set;
 
--- jpa-task5/src/main/resources/sql/alter_tables.sql
+++
@@ -1,8 +0,0 @@
-/*alter table employees
-add role_id int NOT NULL default 1;
-
-alter table employees
-add foreign key (role_id) references roles(id);
-
-alter table project_employee
-drop role_id;*/
