2ad924d3e9322a2487dc0c8afad02d43f738811c
Mohamed Adel <Mohamed.adel90211@gmail.com>
2021-10-11 04:34:14 +0200

    Created database using mysql

---
+++ data_insertion.sql
@@ -0,0 +1,35 @@
+/*INSERT INTO employees
+(full_name, email, phone, age, national_ID)
+VALUES
+("Mohamed Adel",	"ma@gmail.com",	"1134567891",	"30",	"29945612345678"),
+("Emad Zakaria", "ez@gmail.com", "1114567891", 30, "29945612345673"),
+("Mostafa Khaled", "mostafa@gmail.com", "1034567891", 25, "29945612345677"),
+("Ahmed Zaki", "ziko@gmail.com", "1234567891", 23, "29945612345676"),
+("Yasser Khaled", "yk@gmail.com", "1044567891", 28, "29945612345675"),
+("Mazen Hazem", "mh@gmail.com", "1054567891", 25, "29945612345674");
+
+INSERT INTO projects
+(project_name, start_date, project_manager)
+VALUES
+("ABC", "2020-11-25", 1),
+("XYZ", "2021-4-4", 2),
+("Dashmala", "2021-9-4", 3);
+
+INSERT INTO roles
+(role_name, department)
+VALUES
+("developer", "SE"),
+("tester", "SE"),
+("project manager", "SE"),
+("talent acquisition", "hr"),
+("product marketing manager", "Marketing and Communications");
+
+INSERT INTO project_employee
+(employee_id, project_id, role_id)
+VALUES
+(1, 1, 3),
+(2, 2, 3),
+(3, 3, 3),
+(4, 1, 1),
+(5, 1, 2),
+(6, 1, 5);*/
